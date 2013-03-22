cd20f9922b6870e412c7bfc2ce86d9aa8c62f010
tung nguyen <tungnp92@gmail.com>
2013-03-22 18:33:27 +0700

    Initial Commit

---
+++ ExerciseNo2Day8/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ExerciseNo2Day8
+//
+//  Created by stevie nguyen on 3/22/13.
+//  Copyright (c) 2013 tung nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ExerciseNo2Day8/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ExerciseNo2Day8
+//
+//  Created by stevie nguyen on 3/22/13.
+//  Copyright (c) 2013 tung nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
